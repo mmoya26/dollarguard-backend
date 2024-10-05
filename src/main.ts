3bb4d226a87f0394836b48204a4e33edd081848d
mmoya26 <mmoya18@icloud.com>
2024-10-05 15:17:41 -0500

    Update cors origin urls

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://dollarguard.pages.dev', 'http://localhost:4200'],  // Might need to change backend prod url from https to http
+    origin: ['https://dollarguard.pages.dev', 'https://dollarguard-backend-production.up.railway.app/', 'http://localhost:4200'],  // Might need to change backend prod url from https to http
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     preflightContinue: false
