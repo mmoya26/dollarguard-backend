bfb089b98a11564f55f99078183461e6f99236d7
mmoya26 <mmoya18@icloud.com>
2024-10-05 19:37:16 -0500

    readd cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://dollarguard.pages.dev', 'http://localhost:4200'],
+    origin: ['https://dollarguard.pages.dev', 'http://localhost:4200'],  // Might need to change backend prod url from https to http
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     preflightContinue: false
   });
 
   app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
   await app.listen(process.env.PORT);
+
 }
 
 bootstrap();
